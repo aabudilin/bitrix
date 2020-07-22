2eb4983d38031da8acf7b0ad022578a101c9e250
Anton Budylin <dev@pushe.ru>
2020-07-22 09:55:44 +0300

    fix template

--- templates/multilevel_menu/template.php
+++ templates/multilevel_menu/template.php
@@ -1,4 +1,4 @@
-</li><?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 <?//print_r($arResult);?>
 
 <style>
